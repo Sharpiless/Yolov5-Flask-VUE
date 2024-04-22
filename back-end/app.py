54073bafadde1e07434aec8d3fdeb7cffcf28140
Sharpiless <50802787+Sharpiless@users.noreply.github.com>
2024-04-22 09:56:25 +0800

    Update app.py

--- back-end/app.py
+++ back-end/app.py
@@ -78,13 +78,6 @@ def show_photo(file):
 
 
 if __name__ == '__main__':
-    files = [
-        'uploads', 'tmp/ct', 'tmp/draw',
-        'tmp/image', 'tmp/mask', 'tmp/uploads'
-    ]
-    for ff in files:
-        if not os.path.exists(ff):
-            os.makedirs(ff)
     with app.app_context():
         current_app.model = Detector()
     app.run(host='127.0.0.1', port=5003, debug=True)
